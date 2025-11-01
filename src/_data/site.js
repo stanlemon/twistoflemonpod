5e6cf7baff15b0d16b39b524d1e87de815f6ea13
Stan Lemon <86314+stanlemon@users.noreply.github.com>
2025-11-01 19:07:30 -0400

    Update site.js

--- src/_data/site.js
+++ src/_data/site.js
@@ -12,7 +12,7 @@ export default {
   podcast: {
     spotify: "https://open.spotify.com/show/6SVkogELygR53ixQ2T5ECI",
     apple: "https://podcasts.apple.com/us/podcast/life-with-a-twist-of-lemon/id1419902908",
-    youtubeMusic: "", // TODO: Add YouTube Music URL
+    youtubeMusic: "https://www.youtube.com/playlist?list=PLYHJG3ivzy2s0O4hGRRIYxJXSe2YodxcE",
     iheart: "https://www.iheart.com/podcast/256-life-with-a-twist-of-lemon-31050671/",
     tunein: "https://tunein.com/podcasts/Technology-Podcasts/Life-with-a-Twist-of-Lemon-p1147033/",
     overcast: "overcast://x-callback-url/add?url=https%3A%2F%2Ftwistoflemonpod.com%2Ffeed.xml"
